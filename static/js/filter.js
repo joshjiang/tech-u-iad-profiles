1012854398c045d17b8e841e8dcb9671172995a9
Josh Jiang <josh.jiang.dbf@gmail.com>
2019-11-14 15:02:18 -0500

    breakpoints for name and title, removed row pack for list

--- static/js/filter.js
+++ static/js/filter.js
@@ -7,9 +7,23 @@ $(document).ready(
             });
         });
         $(function () {
-            $(".dropdown a").click(function () {
-                $("button:first-child").text($(this).text());
-                $("button:first-child").val($(this).text());
+            $('#role-dropdown a').on('click', function () {
+                let value = $(this).text();
+                $("#profiles .col-md-4").filter(function () {
+                    $(this).toggle($(this).text().includes(value));
+                });
+            });
+        });
+        $(function () {
+            $("#location-dropdown a").click(function () {
+                $("#location-dropdown > button").text($(this).text());
+                $("#location-dropdown > button").val($(this).text());
+            });
+        });
+        $(function () {
+            $("#role-dropdown a").click(function () {
+                $("#role-dropdown > button").text($(this).text());
+                $("#role-dropdown > button").val($(this).text());
             });
         });
     });
